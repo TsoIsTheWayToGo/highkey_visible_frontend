4b0ae1a9fe08f315f80d36986440f21b7d877f5c
TsoIsTheWayToGo <ericgordontso@gmail.com>
2025-06-09 09:39:43 -0500

    add protected route

--- src/components/common/ProtectedRoute.jsx
+++ src/components/common/ProtectedRoute.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import useAuthStore from '../../store/authStore';
 import AuthModal from '../auth/AuthModal';
 
 const ProtectedRoute = ({ children }) => {
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  const [showAuthModal, setShowAuthModal] = useState(!isAuthenticated);
+
+  const handleAuthSuccess = () => {
+    setShowAuthModal(false);
+  };
 
   if (!isAuthenticated) {
-    return <AuthModal isOpen={true} onClose={() => {}} />;
+    return (
+      <AuthModal 
+        isOpen={showAuthModal} 
+        onClose={() => setShowAuthModal(false)}
+        onSuccess={handleAuthSuccess}
+      />
+    );
   }
 
   return children;
